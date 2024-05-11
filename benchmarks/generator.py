ab4067ba08482b9d4fa2b14ad0911a07b85c83e0
DavidMunozValero <david.munoz6@alu.uclm.es>
2024-05-11 11:14:40 +0200

    feat: update canon

--- benchmarks/generator.py
+++ benchmarks/generator.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+from geopy.distance import geodesic
+from robin.supply.entities import Supply
 from scipy.stats import loguniform
-from typing import List, Mapping, Union
+from typing import List, Mapping, Tuple, Union
 
 
 def get_lines(corridor: Mapping[str, Mapping],
@@ -104,26 +106,35 @@ def get_schedule_request(corridor: Mapping[str, Mapping],
     return schedule_request
 
 
-def get_revenue_behaviour(schedule: Mapping[str, Mapping[str, List[float]]]
-                          ) -> Mapping[str, Mapping[str, float]]:
+def get_revenue_behaviour(supply: Supply,
+                          alpha: float = 2/3) -> Mapping[str, Mapping[str, float]]:
     """
     Get revenue behaviour
 
     Args:
-        schedule (Mapping[int, Mapping[str, List[float]]]): schedule.
+        supply (Supply): supply.
+        alpha (float): alpha to adjust canon by 100 passengers.
 
     Returns:
         Mapping[int, Mapping[str, float]]: revenue behaviour.
     """
     revenue = {}
-    bias = [0.2, 0.35, 0.1]
-    for service in schedule:
-        b = np.random.choice(bias)
-        base_price = 55 * len(schedule[service])
-        canon = base_price + b * base_price
-        k = np.round(loguniform.rvs(0.01, 100, 1), 2)
-        max_penalty = canon * 0.4
+    tsp_k = {}
+    for service in supply.services:
+        sta_coords = [sta.coords for sta in service.line.stations]
+        distances = [geodesic(sta_coords[i], sta_coords[i+1]).km for i in range(len(sta_coords) - 1)]
+        distance_factor = 7 * sum(distances)
+        service_capacity = service.rolling_stock.total_capacity
+        capacity_factor = alpha * service_capacity
+        stations_factor = 180 + (len(sta_coords) - 2) * 65 + 165
+        total_canon = distance_factor + capacity_factor + stations_factor
+        max_penalty = total_canon * 0.4
         dt_penalty = np.round(max_penalty * 0.35, 2)
-        tt_penalty = np.round((max_penalty - dt_penalty) / (len(schedule[service]) - 1), 2)
-        revenue[service] = {'canon': canon, 'k': k, 'dt_max_penalty': dt_penalty, 'tt_max_penalty': tt_penalty}
+        tt_penalty = np.round((max_penalty - dt_penalty) / (len(sta_coords) - 1), 2)
+        if service.tsp.id not in tsp_k:
+            k = np.round(loguniform.rvs(0.01, 100, 1), 2)
+            tsp_k[service.tsp.id] = k
+        else:
+            k = tsp_k[service.tsp.id]
+        revenue[service.id] = {'canon': total_canon, 'k': k, 'dt_max_penalty': dt_penalty, 'tt_max_penalty': tt_penalty}
     return revenue
